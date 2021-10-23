3952919d34ab18e49d567e1b4fccdd9fcb8857ae
erwinarif31 <muherwinarif31@gmail.com>
2021-10-24 05:40:46 +0800

    feat(src/P0201): initialize `start`, `end`, `size` variable as input variable

--- src/P0201.java
+++ src/P0201.java
@@ -4,6 +4,11 @@ public class P0201 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int start, end, size;
+        start = scan.nextInt();
+        end = scan.nextInt();
+        size = scan.nextInt();
         
     }
 }
