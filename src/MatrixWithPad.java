436ffca643af9917ad88f9ffbd5be9285f5fe632
erwinarif31 <muherwinarif31@gmail.com>
2021-10-24 06:57:19 +0800

    feat(src/MatrixWithPad): add `if else` statement to handle output

---
+++ src/MatrixWithPad.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MatrixWithPad {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int start, end, size;
+        
+        try {
+            start = scan.nextInt();
+            end = scan.nextInt();
+            size = scan.nextInt();
+        } catch (InputMismatchException e) {
+            //TODO: handle exception
+            System.out.println("Masukkan input integer");
+            return;
+        }
+
+        for (int i = start, j = 1; (start < end) ? i <= end : i >= end; j++) {
+            System.out.printf("%03d ", i);
+            if (j % size == 0) {
+                System.out.println();
+            }
+            if (start < end) {
+                i++;
+            } else {
+                i--;
+            }
+        }
+
+    }
+}
